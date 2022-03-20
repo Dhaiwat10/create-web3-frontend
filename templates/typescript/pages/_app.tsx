e07e163237819000567e06b9e3b6b0b3ebd7b79c
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2022-03-21 05:12:08 +0530

    Add Typescript template

---
+++ templates/typescript/pages/_app.tsx
@@ -0,0 +1,71 @@
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Connector, Provider, chain, defaultChains } from 'wagmi';
+import { InjectedConnector } from 'wagmi/connectors/injected';
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
+import { WalletLinkConnector } from 'wagmi/connectors/walletLink';
+import { providers } from 'ethers';
+
+// Get environment variables
+const infuraId = process.env.NEXT_PUBLIC_INFURA_ID as string;
+
+// Pick chains
+const chains = defaultChains;
+const defaultChain = chain.mainnet;
+
+// Set up connectors
+type ConnectorsConfig = { chainId?: number };
+const connectors = ({ chainId }: ConnectorsConfig) => {
+  const rpcUrl =
+    chains.find((x) => x.id === chainId)?.rpcUrls?.[0] ??
+    defaultChain.rpcUrls[0];
+  return [
+    new InjectedConnector({ chains }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        infuraId,
+        qrcode: true,
+      },
+    }),
+    new WalletLinkConnector({
+      chains,
+      options: {
+        appName: 'dForms',
+        jsonRpcUrl: `${rpcUrl}/${infuraId}`,
+      },
+    }),
+  ];
+};
+
+// Set up providers
+type ProviderConfig = { chainId?: number; connector?: Connector };
+const isChainSupported = (chainId?: number) =>
+  chains.some((x) => x.id === chainId);
+
+const provider = ({ chainId }: ProviderConfig) =>
+  providers.getDefaultProvider(
+    isChainSupported(chainId) ? chainId : defaultChain.id,
+    {
+      infuraId,
+    }
+  );
+const webSocketProvider = ({ chainId }: ProviderConfig) =>
+  isChainSupported(chainId)
+    ? new providers.InfuraWebSocketProvider(chainId, infuraId)
+    : undefined;
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Provider
+      autoConnect
+      connectors={connectors}
+      provider={provider}
+      webSocketProvider={webSocketProvider}
+    >
+      <Component {...pageProps} />
+    </Provider>
+  );
+}
+
+export default MyApp;
