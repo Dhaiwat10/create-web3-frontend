de4e5b7420171e40e7dd6fb91ed9caa4c4c1ed0a
Dhaiwat / dhai.eth <dhaiwatpandya@gmail.com>
2023-04-06 05:40:19 +0530

    rework

--- index.ts
+++ index.ts
@@ -1,193 +1,73 @@
-#!/usr/bin/env node
-/* eslint-disable import/no-extraneous-dependencies */
-import chalk from 'chalk';
-import Commander from 'commander';
-import path from 'path';
-import prompts from 'prompts';
-import checkForUpdate from 'update-check';
-import { createApp, DownloadError } from './create-app';
-import { getPkgManager } from './helpers/get-pkg-manager';
-import { validateNpmName } from './helpers/validate-pkg';
+import { Command } from 'commander';
 import packageJson from './package.json';
+import chalk from 'chalk';
+import { mkdir, unlink } from 'fs/promises';
+import tar from 'tar';
+import { join } from 'path';
+import { tmpdir } from 'os';
+import { pipeline } from 'stream/promises';
+import got from 'got';
+import { createWriteStream } from 'fs';
+import prompts from 'prompts';
 
-let projectPath: string = '';
-
-const program = new Commander.Command(packageJson.name)
-  .version(packageJson.version)
-  .arguments('<project-directory>')
-  .usage(`${chalk.green('<project-directory>')} [options]`)
-  .action((name) => {
-    projectPath = name;
-  })
-  .option(
-    '--ts, --typescript',
-    `
-  Initialize as a TypeScript project.
-`
-  )
-  .option(
-    '--chakra, --chakra-ui',
-    `
-    Use Chakra UI instead of Tailwind.
-    `
-  )
-  .option(
-    '--use-npm',
-    `
-  Explicitly tell the CLI to bootstrap the app using npm
-`
-  )
-  .option(
-    '--use-pnpm',
-    `
-  Explicitly tell the CLI to bootstrap the app using pnpm
-`
-  )
-  .allowUnknownOption()
-  .parse(process.argv);
-
-async function run(): Promise<void> {
-  if (typeof projectPath === 'string') {
-    projectPath = projectPath.trim();
-  }
-  if (!projectPath) {
-    const res = await prompts({
-      type: 'text',
-      name: 'path',
-      message: 'What is your project named?',
-      initial: 'my-app',
-      validate: (name) => {
-        const validation = validateNpmName(path.basename(path.resolve(name)));
-        if (validation.valid) {
-          return true;
-        }
-        return 'Invalid project name: ' + validation.problems![0];
-      },
-    });
-
-    if (typeof res.path === 'string') {
-      projectPath = res.path.trim();
-    }
-  }
-
-  if (!projectPath) {
-    console.log();
-    console.log('Please specify the project directory:');
-    console.log(
-      `  ${chalk.cyan(program.name())} ${chalk.green('<project-directory>')}`
-    );
-    console.log();
-    console.log('For example:');
-    console.log(
-      `  ${chalk.cyan(program.name())} ${chalk.green('my-next-app')}`
-    );
-    console.log();
-    console.log(
-      `Run ${chalk.cyan(`${program.name()} --help`)} to see all options.`
-    );
-    process.exit(1);
-  }
-
-  const resolvedProjectPath = path.resolve(projectPath);
-  const projectName = path.basename(resolvedProjectPath);
-
-  const { valid, problems } = validateNpmName(projectName);
-  if (!valid) {
-    console.error(
-      `Could not create a project called ${chalk.red(
-        `"${projectName}"`
-      )} because of npm naming restrictions:`
-    );
-
-    problems!.forEach((p: any) =>
-      console.error(`    ${chalk.red.bold('*')} ${p}`)
-    );
-    process.exit(1);
-  }
-
-  const packageManager = !!program.useNpm
-    ? 'npm'
-    : !!program.usePnpm
-    ? 'pnpm'
-    : 'yarn';
-
-  try {
-    await createApp({
-      appPath: resolvedProjectPath,
-      packageManager,
-      typescript: program.typescript,
-      cssLibrary: program.chakraUi ? 'chakra-ui' : 'tailwind',
-    });
-  } catch (reason) {
-    if (!(reason instanceof DownloadError)) {
-      throw reason;
-    }
-
-    const res = await prompts({
-      type: 'confirm',
-      name: 'builtin',
-      message:
-        `Could not download because of a connectivity issue between your machine and GitHub.\n` +
-        `Do you want to use the default template instead?`,
-      initial: true,
-    });
-    if (!res.builtin) {
-      throw reason;
-    }
-
-    await createApp({
-      appPath: resolvedProjectPath,
-      packageManager,
-      typescript: program.typescript,
-      cssLibrary: program.chakraUi ? 'chakra-ui' : 'tailwind',
-    });
-  }
+async function downloadTar(url: string) {
+  const tempFile = join(tmpdir(), `create-web3-frontend.temp-${Date.now()}`);
+  await pipeline(got.stream(url), createWriteStream(tempFile));
+  return tempFile;
 }
 
-const update = checkForUpdate(packageJson).catch(() => null);
-
-async function notifyUpdate(): Promise<void> {
-  try {
-    const res = await update;
-    if (res?.latest) {
-      const pkgManager = getPkgManager();
-
-      console.log();
-      console.log(
-        chalk.yellow.bold(
-          'A new version of `create-web3-frontend` is available!'
-        )
-      );
-      console.log(
-        'You can update by running: ' +
-          chalk.cyan(
-            pkgManager === 'yarn'
-              ? 'yarn global add create-web3-frontend'
-              : `${pkgManager} install --global create-web3-frontend`
-          )
-      );
-      console.log();
-    }
-    process.exit();
-  } catch {
-    // ignore error
-  }
-}
-
-run()
-  .then(notifyUpdate)
-  .catch(async (reason) => {
-    console.log();
-    console.log('Aborting installation.');
-    if (reason.command) {
-      console.log(`  ${chalk.cyan(reason.command)} has failed.`);
-    } else {
-      console.log(chalk.red('Unexpected error. Please report it as a bug:'));
-      console.log(reason);
-    }
-    console.log();
-
-    await notifyUpdate();
-
-    process.exit(1);
+async function getUserInput() {
+  const response = await prompts({
+    type: 'text',
+    name: 'input',
+    message: 'Project name:',
   });
+
+  return response.input;
+}
+
+const main = async () => {
+  let projectPath = '';
+  const program = new Command(packageJson.name)
+    .version(packageJson.version)
+    .arguments('<project-directory>')
+    .usage(`${chalk.green('<project-directory>')} [options]`)
+    .action((name) => {
+      projectPath = name;
+    })
+    .parse(process.argv);
+
+  if (!projectPath) {
+    projectPath = await getUserInput();
+  }
+
+  await mkdir(projectPath);
+  const tempFile = await downloadTar(
+    `https://codeload.github.com/Dhaiwat10/cw3f-new-template/tar.gz/main`
+  );
+  await tar.x({
+    file: tempFile,
+    strip: 1,
+    cwd: join(process.cwd(), projectPath),
+  });
+  await unlink(tempFile);
+
+  console.log();
+  console.log();
+  console.log(
+    chalk.green('⚡️ Success! Created a web3 frontend at ' + projectPath)
+  );
+  console.log();
+  console.log();
+  console.log('To get started:');
+  console.log();
+  console.log(`- cd into the project directory: cd ${projectPath}`);
+  console.log('- Install dependencies by running `npm install`');
+  console.log('- Run the dev server by running `npm run dev`');
+  console.log();
+  console.log();
+};
+
+main().catch((err) => {
+  console.error(err);
+});
